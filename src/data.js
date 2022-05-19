7e750489041dbfa4dee72100a516d823f020536d
Sam Smith <korhan.tokgoz92@gmail.com>
2022-05-20 02:23:18 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Korhan Tokgöz',
+    age: 29,
+    image:
+      '../image/IMG_5081.JPEG',
+  },
+  {
+    id: 2,
+    name: 'Mustafa Kemal Atatürk',
+    age: 57 ,
+    image:
+      '../image/images.jpg',
+  },
+  {
+    id: 3,
+    name: 'George Washington',
+    age: 67,
+    image:
+      '../image/George Washington.jpg',
+  },
+  {
+    id: 4,
+    name: 'Robert Walpole',
+    age: 69,
+    image:
+      '../image/Robert Walpole.jpg',
+  },
+  {
+    id: 5,
+    name: 'Otto von Bismarck',
+    age: 83,
+    image:
+      '../image/Otto von Bismarck.jpg',
+  },
+];
