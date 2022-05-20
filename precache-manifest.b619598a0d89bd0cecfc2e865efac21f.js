1948fa3643a1fe48bdc0534677f2634fdaaff187
Sam Smith <korhan.tokgoz92@gmail.com>
2022-05-20 21:51:54 +0300

    Updates

---
+++ precache-manifest.b619598a0d89bd0cecfc2e865efac21f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c718ad106be3d6bb526366fec7d927",
+    "url": "/Birthday-Reminder-Project/index.html"
+  },
+  {
+    "revision": "4e28d9f8dce818b9925c",
+    "url": "/Birthday-Reminder-Project/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/Birthday-Reminder-Project/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Birthday-Reminder-Project/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e28d9f8dce818b9925c",
+    "url": "/Birthday-Reminder-Project/static/js/main.6e4eaeb0.chunk.js"
+  },
+  {
+    "revision": "f27370eed9b59f592bf7",
+    "url": "/Birthday-Reminder-Project/static/js/runtime-main.fa204f06.js"
+  }
+]);
